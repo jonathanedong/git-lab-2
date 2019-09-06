768c9c595a256581a06b104d587cae4cbe0c91ea
JonathanDong <jd964917@ohio.edu>
2019-09-06 14:58:47 -0400

    Initial commit c++ program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *          File: git-lab-program.cc
+ *        Author: Jonathan Dong
+ *          Date: September 6, 2019
+ *   Description: A C++ Program in git-lab2 repository for lab 2.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+} // main
